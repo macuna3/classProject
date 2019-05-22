c6dc39a301c1baaf13e89990f919a38ef5568463
Maria <sacatum@hotmail.com>
2019-05-22 13:31:53 +0200

    starting with the client part

---
+++ WebClient/src/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import agenciawebservice.Agencia_Service;
+import agenciawebservice.Exception_Exception;
+import agenciawebservice.Reserva;
+import agenciawebservice.Titular;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ *
+ * @author Maluz
+ */
+@WebServlet(urlPatterns = {"/Cliente"})
+public class Cliente extends HttpServlet {
+
+}
