25af71c5da7cd75dd06b2208af7523a4284b0ec6
Alex <juasjuas82@gmail.com>
2019-05-22 13:53:18 +0200

    finishing agencia

--- WebServer/src/java/Agencia.java
+++ WebServer/src/java/Agencia.java
@@ -14,7 +14,7 @@ import javax.jws.WebParam;
 
 /**
  *
- * @author Maluz and Alex
+ * @author Maluz y Alex
  */
 @WebService(serviceName = "Agencia")
 public class Agencia {
@@ -31,23 +31,130 @@ public class Agencia {
                           String destino, Titular titular) throws Exception
   {
 
+    
+    if (titular != null)
+      {
+        Reserva newreserva = new Reserva();
+          newreserva.setNumReserva(numreservas);
+          newreserva.setTitular(titular);
+          newreserva.setIda(ida);
+          newreserva.setVuelta(vuelta);
+          newreserva.setOrigen(origen);
+          newreserva.setDestino(destino);
+          reservas.add(newreserva);
+          
+      }
+    else
+      {
+        throw new Exception("Titular invalido!");
+      }
+    numreservas++;
+    return numreservas-1;
   }
 
   public void cancelarReserva(int numreserva,
                               Titular titular) throws Exception
   {
+  int encontrado=0;
+    
 
+      for (int i = 0; i < reservas.size(); i++)
+        {
+
+          Reserva n = (Reserva) reservas.get(i);
+
+          if (n.getNumReserva() == numreserva)
+            {
+              if (n.getTitular().getNombre() == titular.getNombre())
+                {
+                  reservas.remove(i);
+                  System.out.println("Se ha eliminado la reserva");
+                  encontrado=1;
+                }
+              else
+                {
+                  throw new
+                    Exception
+                    ("El titular proporcionado y el de la reserva no coinciden!");
+                }
+            }
+        }
+    if (encontrado==0)
+    {
+    throw new Exception("No existe una reserva con ese numero!");
+    }
   }
 
   public void modificarReserva(int num, int control,
                                Date nueva) throws Exception
   {
 
+    int encontrado=0;
+      for (int i = 0; i < reservas.size(); i++)
+        {
+
+          Reserva n = (Reserva) reservas.get(i);
+
+          if (num == n.getNumReserva())
+            {
+              encontrado=1;
+              if (control == 1)
+                {
+                  n.setIda(nueva); 
+                  System.out.println("Se ha modificado la fecha de ida");
+                }
+              else
+                {
+                  n.setVuelta(nueva);
+                  System.out.println("Se ha modificado la fecha de vuelta");
+                }
+            }
+        }
+
+    
+    if(encontrado==0)
+    {
+      throw new Exception("No existe una reserva con ese numero!");
+    }
+
   }
 
 
   public Reserva[] ReservasDelTitular(String dni) throws Exception
   {
+
+    Vector v = new Vector();
+
+
+    for (int i = 0; i < reservas.size(); i++)
+      {
+
+        Reserva n = (Reserva) reservas.get(i);
+
+        if (n.getTitular().getDni().indexOf(dni) != -1)
+          {
+            v.add(n);
+          }
+      }
+
+    if (v.size() > 0)
+      {
+
+        Reserva vn[] = new Reserva[v.size()];
+
+        for (int k = 0; k < v.size(); k++)
+          {
+            vn[k] = (Reserva) v.get(k);
+          }
+
+        return vn;
+
+      }
+    else
+      throw new Exception("DNI no encontrado.");
+
+  }
+
 
     
 }
